6d360e71f6e82a7f0441546509e5bec4b56abcd6
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-08-16 22:47:40 -0300

    refatoração

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const shoppingCartClass = '.cart__items';
+const shoppingCartID = '#shopping-cart';
+
+function addShoppingCartToStorage() {
+  const shoppingCart = document.querySelector(shoppingCartID);
+  window.localStorage.setItem('shopping_cart', shoppingCart.innerHTML);
+}
+
+function fetchCartInStorage() {
+  const shoppingCart = document.querySelector(shoppingCartID);
+  shoppingCart.innerHTML = localStorage.getItem('shopping_cart');
+}
+
+// ------------------------------------------------------------------------------------
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -32,76 +44,76 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function addShoppingCartToStorage() {
-  const shoppingCart = document.querySelector(shoppingCartClass);
-  window.localStorage.setItem('shopping_cart', shoppingCart.innerHTML);
-}
-
-function cartItemClickListener(event) {
-  event.target.remove();
+async function cartItemClickListener(event) {
+  await event.target.remove();
   addShoppingCartToStorage();
-}
-
-function addCartListener() {
-  const cartItems = document.querySelectorAll('.cart__item');
-  cartItems.forEach((item) => {
-    item.addEventListener('click', cartItemClickListener);
-  });
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
+  li.id = sku;
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
-function fetchResults(product) {
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+// ------------------------------------------------------------------------------------
+
+function fetchResults(searchTerm) {
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchTerm}`)
     .then((response) => response.json())
     .then((response) =>
       response.results.map((item) => ({
         sku: item.id,
         name: item.title,
         image: item.thumbnail,
+        // salePrice: item.price,
       })))
-    .then((results) => {
+    .then((results) =>
       results.forEach((result) => {
         document
           .querySelector('.items')
           .appendChild(createProductItemElement(result));
-      });
-    });
+      }));
 }
 
 function fetchItemById(id) {
-  fetch(`https://api.mercadolibre.com/items/${id}`)
+  return fetch(`https://api.mercadolibre.com/items/${id}`)
     .then((response) => response.json())
-    .then((item) => ({ sku: item.id, name: item.title, salePrice: item.price }))
-    .then((item) =>
-      document
-        .querySelector(shoppingCartClass)
-        .appendChild(createCartItemElement(item)))
-    .then(() => addShoppingCartToStorage());
+    .then((item) => ({
+      sku: item.id,
+      name: item.title,
+      salePrice: item.price,
+    }));
+}
+
+function appendItem(item) {
+  document
+    .querySelector(shoppingCartID)
+    .appendChild(createCartItemElement(item));
 }
 
 function addProductToCart() {
   document.querySelectorAll('.item__add').forEach((item) =>
-    item.addEventListener('click', (event) => {
+    item.addEventListener('click', async (event) => {
       const itemID = getSkuFromProductItem(event.target.parentElement);
-      fetchItemById(itemID);
+      const mappedItem = await fetchItemById(itemID);
+      appendItem(mappedItem);
+      addShoppingCartToStorage();
     }));
 }
 
-function fetchCartInStorage() {
-  const shoppingCart = document.querySelector(shoppingCartClass);
-  shoppingCart.innerHTML = localStorage.getItem('shopping_cart');
+function removeProductFromCart() {
+  const cartItems = document.querySelectorAll(shoppingCartID);
+  cartItems.forEach((item) => {
+    item.addEventListener('click', cartItemClickListener);
+  });
 }
 
 window.onload = async () => {
   await fetchResults('computador');
   fetchCartInStorage();
   addProductToCart();
-  addCartListener();
+  removeProductFromCart();
 };
